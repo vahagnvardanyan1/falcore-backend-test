c7d248be0be6619ac817dce3882a884b32c6e488
Vahagn Vardanyan <vahagn.vardanyan2@picsart.com>
2026-02-19 22:56:18 +0400

    ui for api tests

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
+  serverExternalPackages: ["@playwright/test", "playwright"],
 };
 
 export default nextConfig;
