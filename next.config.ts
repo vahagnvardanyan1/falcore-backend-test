a7e473902e321c6bd373f9d8f46e936fdfaa8a15
Vahagn Vardanyan <vahagn.vardanyan2@picsart.com>
2026-02-19 23:03:45 +0400

    removed ui for api tests

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
-  serverExternalPackages: ["@playwright/test", "playwright"],
 };
 
 export default nextConfig;
