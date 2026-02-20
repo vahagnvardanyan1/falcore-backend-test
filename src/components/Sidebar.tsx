3e4f169a1560b87819cf24088438da1c15edfe71
Vahagn Vardanyan <vahagn.vardanyan2@picsart.com>
2026-02-20 15:38:50 +0400

    removed sidebars test runner route

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -16,7 +16,6 @@ const NAV_ITEMS = [
   { href: "/vehicle-insurances", label: "Insurances" },
   { href: "/vehicle-parts", label: "Vehicle Parts" },
   { href: "/vehicle-inspections", label: "Inspections" },
-  { href: "/test-runner", label: "Test Runner" },
 ];
 
 export default function Sidebar() {
